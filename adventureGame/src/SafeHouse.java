69d3bb09cdb05e9ec73f7ba6761bba9b657e5dae
Mahmut Ali Şahinkaya <107810135+MahmutAliSahinkaya@users.noreply.github.com>
2022-10-24 10:50:49 +0300

    Add files via upload

---
+++ adventureGame/src/SafeHouse.java
@@ -0,0 +1,16 @@
+public class SafeHouse extends NormalLoc{
+    public SafeHouse(Player player) {
+        super(player, "Güvenli Ev");
+    }
+
+
+    @Override
+    public boolean onLocation() {
+        System.out.println("Güvenli evdesiniz !");
+        System.out.println("Canınız yenilendi !");
+        this.getPlayer().setHealth(this.getPlayer().getOrjinalHealth());
+        return true;
+    }
+
+
+}
