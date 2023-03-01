b596f177d74799ea73c33459a91de64f264c4782
Mahmut Ali Şahinkaya <107810135+MahmutAliSahinkaya@users.noreply.github.com>
2023-03-01 15:56:11 +0300

    Add files via upload

---
+++ quickSort/src/QuickSort.java
@@ -0,0 +1,49 @@
+public class QuickSort {
+    public static void sort(int[] array) {
+        if (array == null || array.length == 0) {
+            return;
+        }
+        quickSort(array, 0, array.length - 1);
+    }
+
+    private static void quickSort(int[] array, int left, int right) {
+        if (left >= right) {
+            return;
+        }
+        int pivot = array[(left + right) / 2];
+        int index = partition(array, left, right, pivot);
+        quickSort(array, left, index - 1);
+        quickSort(array, index, right);
+    }
+
+    private static int partition(int[] array, int left, int right, int pivot) {
+        while (left <= right) {
+            while (array[left] < pivot) {
+                left++;
+            }
+            while (array[right] > pivot) {
+                right--;
+            }
+            if (left <= right) {
+                swap(array, left, right);
+                left++;
+                right--;
+            }
+        }
+        return left;
+    }
+
+    private static void swap(int[] array, int i, int j) {
+        int temp = array[i];
+        array[i] = array[j];
+        array[j] = temp;
+    }
+
+    public static void main(String[] args) {
+        int[] array = { 1, 5, 3, 2, 4, 6, 7, 8, 9, 10 };
+        sort(array);
+        for (int i : array) {
+            System.out.print(i + " ");
+        }
+    }
+}
